9dbaf297e9ffc1da80e437eec126b743e9e4ffc0
Tatiana Ivanova <tat.mi.iva@gmail.com>
2023-06-07 16:23:44 +0300

    feat: create components for the landing page

---
+++ src/components/AboutMe/AboutMe.jsx
@@ -0,0 +1,64 @@
+import { Link } from 'react-router-dom';
+
+import './AboutMe.css';
+
+function AboutMe() {
+  return (
+    <section className='about-me'>
+      <div className='container container_place_about-me'>
+        <h2 className='landing-title about-me__title'>Студент</h2>
+
+        <article className='brief'>
+          <h3 className='brief__title'>Татьяна</h3>
+          <p className='brief__subtitle'>Фронтенд-разработчик, 33 года</p>
+          <p className='brief__desc'>
+            Я из Петербурга, закончила филологический факультет РГПУ им. А.И.
+            Герцена. В 2020 году я увлеклась вёрсткой, а с июня 2022 года
+            полностью посвящаю всё своё время изучению программирования.
+          </p>
+          <Link
+            to='https://github.com/Tatty13'
+            target='_blank'
+            className='link brief__link'>
+            Github
+          </Link>
+          <img
+            src='https://avatars.githubusercontent.com/u/91495653?v=4'
+            alt='me'
+            className='brief__img'
+          />
+        </article>
+        <article className='portfolio'>
+          <h3 className='portfolio__title'>Портфолио</h3>
+          <ol className='list portfolio__list'>
+            <li className='portfolio__item'>
+              <Link
+                to='https://tatty13.github.io/how-to-learn/'
+                target='_blank'
+                className='link portfolio__link'>
+                Статичный сайт
+              </Link>
+            </li>
+            <li className='portfolio__item'>
+              <Link
+                to='https://tatty13.github.io/russian-travel/'
+                target='_blank'
+                className='link portfolio__link'>
+                Адаптивный сайт
+              </Link>
+            </li>
+            <li className='portfolio__item'>
+              <Link
+                to='https://tatty13.github.io/react-mesto-auth/'
+                target='_blank'
+                className='link portfolio__link'>
+                Одностраничное приложение
+              </Link>
+            </li>
+          </ol>
+        </article>
+      </div>
+    </section>
+  );
+}
+export { AboutMe };
---
+++ src/components/Promo/Promo.jsx
@@ -0,0 +1,26 @@
+import { HashLink } from 'react-router-hash-link';
+import './Promo.css';
+
+function Promo() {
+  return (
+    <section className='promo'>
+      <div className='container container_place_promo'>
+        <h1 className='promo__title'>
+          Учебный проект студента факультета{' '}
+          <span className='promo__nospace'>Веб-разработки.</span>
+        </h1>
+        <p className='promo__subtitle'>
+          Листайте ниже, чтобы узнать больше про этот проект и его создателя.
+        </p>
+        <HashLink
+          smooth
+          to='/#about'
+          className='link promo__link'>
+          Узнать больше
+        </HashLink>
+      </div>
+    </section>
+  );
+}
+
+export { Promo };

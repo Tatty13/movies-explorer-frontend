11f7195ae6b630d446b3d46a98165197c676aada
Tatiana Ivanova <tat.mi.iva@gmail.com>
2023-06-07 16:21:05 +0300

    feat: create not-found page

---
+++ src/pages/NotFound/NotFound.jsx
@@ -0,0 +1,18 @@
+import { Link } from 'react-router-dom';
+import './NotFound.css';
+
+function NotFound() {
+  return (
+    <section className='container container_place_not-found not-found'>
+      <span className='not-found__accent'>404</span>
+      <h1 className='not-found__title'>Страница не найдена</h1>
+      <Link
+        to={'/'}
+        className='link link_type_normal link_theme_brand-secondary'>
+        Назад
+      </Link>
+    </section>
+  );
+}
+
+export { NotFound };

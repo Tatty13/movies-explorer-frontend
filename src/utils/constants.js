c19d720b1a088b055eb43d27ac6deaf1102227ac
Tatiana Ivanova <tat.mi.iva@gmail.com>
2023-06-07 16:16:47 +0300

    refactor: add file with constants

---
+++ src/utils/constants.js
@@ -0,0 +1,11 @@
+const ROUTES_WITH_FOOTER = ['/', '/movies', '/saved-movies', '/profile'];
+const ROUTES_WITH_HEADER = [
+  '/',
+  '/movies',
+  '/saved-movies',
+  '/profile',
+  '/signin',
+  '/signup',
+];
+
+export { ROUTES_WITH_FOOTER, ROUTES_WITH_HEADER };

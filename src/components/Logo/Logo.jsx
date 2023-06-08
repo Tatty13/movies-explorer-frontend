d5749f64cf37a717c053263f0180aa928608e1d8
Tatiana Ivanova <tat.mi.iva@gmail.com>
2023-06-08 22:28:08 +0300

    feat: add logo component

---
+++ src/components/Logo/Logo.jsx
@@ -0,0 +1,15 @@
+import { Link } from 'react-router-dom';
+
+import './Logo.css'
+
+function Logo() {
+  return (
+    <Link
+      to='/'
+      className='logo'
+      aria-label='Логотип'
+    />
+  );
+}
+
+export { Logo };

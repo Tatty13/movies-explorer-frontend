9eea4bf8c50128aff1c94da930fc80681ab7ad2d
Tatiana Ivanova <tat.mi.iva@gmail.com>
2023-06-09 15:14:09 +0300

    feat: create header component

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,50 @@
+import { useLocation } from 'react-router-dom';
+import './Header.css';
+import { ROUTES_WITH_HEADER, ROUTES_WITH_NAVBAR } from '../../utils/constants';
+
+import { Logo } from '../Logo/Logo';
+import { NavBar } from '../NavBar/NavBar';
+import { BurgerBtn } from '../BurgerBtn/BurgerBtn';
+import { useState } from 'react';
+
+function Header() {
+  const { pathname } = useLocation();
+  const [isNavBarOpen, setNavBarState] = useState(false);
+
+  const toggleBurgerMenu = () => setNavBarState(!isNavBarOpen);
+
+  const hasHeader = ROUTES_WITH_HEADER.includes(pathname);
+  const hasNavBar = ROUTES_WITH_NAVBAR.includes(pathname);
+
+  /**
+   * todo - удалить на этапе 3
+   */
+  const isLoggedIn = pathname !== '/';
+
+  return (
+    hasHeader && (
+      <header
+        className={`header ${
+          pathname === '/' ? 'header_theme_hero' : 'header_theme_normal'
+        }`}>
+        <div className='container container_place_header'>
+          <Logo />
+          {hasNavBar && (
+            <NavBar
+              isLoggedIn={isLoggedIn}
+              isOpen={isNavBarOpen}
+            />
+          )}
+          {hasNavBar && isLoggedIn && (
+            <BurgerBtn
+              onClick={toggleBurgerMenu}
+              isOpen={isNavBarOpen}
+            />
+          )}
+        </div>
+      </header>
+    )
+  );
+}
+
+export { Header };

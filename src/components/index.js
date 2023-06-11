b98f54bbe246e584c579aed24d806506b5fbb2bf
Tatiana Ivanova <tat.mi.iva@gmail.com>
2023-06-11 18:52:07 +0300

    refactor: re-export components for the movie page

--- src/components/index.js
+++ src/components/index.js
@@ -7,3 +7,6 @@ export * from './Promo/Promo';
 export * from './AboutProject/AboutProject';
 export * from './Techs/Techs';
 export * from './AboutMe/AboutMe';
+export * from './SearchForm/SearchForm';
+export * from './MoviesCardList/MoviesCardList';
+export * from './MoviesCard/MoviesCard';

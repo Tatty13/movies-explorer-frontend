9d336376ac3d224d92123870a0d630593d0d6dea
Tatiana Ivanova <tat.mi.iva@gmail.com>
2023-06-13 00:16:26 +0300

    feat: create auth page component

---
+++ src/components/AuthPage/AuthPage.jsx
@@ -0,0 +1,40 @@
+import { Link } from 'react-router-dom';
+import { Logo, Form } from '../';
+import './AuthPage.css';
+
+function AuthPage({ title, pageName, btnText, children }) {
+  return (
+    <section className='container section auth'>
+      <Logo />
+      <h1 className='section__title auth__title'>{title}</h1>
+      <Form
+        title={title}
+        name={pageName}
+        btnText={btnText}>
+        {children}
+      </Form>
+      {pageName === 'register' && (
+        <p className='auth__text'>
+          Уже зарегистрированы?{' '}
+          <Link
+            to='/signin'
+            className='link link_theme_brand-secondary auth__link'>
+            Войти
+          </Link>
+        </p>
+      )}
+      {pageName === 'login' && (
+        <p className='auth__text'>
+          Ещё не зарегистрированы?{' '}
+          <Link
+            to='/signup'
+            className='link link_theme_brand-secondary auth__link'>
+            Регистрация
+          </Link>
+        </p>
+      )}
+    </section>
+  );
+}
+
+export { AuthPage };

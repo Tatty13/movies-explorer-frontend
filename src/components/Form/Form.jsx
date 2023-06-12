0e31bfb2096966b668b878544bf7da5c61df5c11
Tatiana Ivanova <tat.mi.iva@gmail.com>
2023-06-13 00:15:26 +0300

    feat: create form component

---
+++ src/components/Form/Form.jsx
@@ -0,0 +1,24 @@
+import './Form.css';
+
+function Form({ name, btnText, onSubmit, isEditMode = true, children }) {
+  return (
+    <form
+      onSubmit={onSubmit}
+      className={`form form_place_${name}`}
+      action='#'
+      name={name}
+      noValidate>
+      {children}
+      <button
+        className={`form__submit-btn ${
+          !isEditMode && 'form__submit-btn_invisible'
+        }`}
+        type='submit'
+        name='submit-btn'>
+        {btnText || 'Сохранить'}
+      </button>
+    </form>
+  );
+}
+
+export { Form };

02473b5e733960b58558bad3bd28830702a3285f
Tatiana Ivanova <tat.mi.iva@gmail.com>
2023-06-13 21:03:04 +0300

    fix: navigate back in the history but not on main page, correct styles due to button tag usage

--- src/pages/NotFound/NotFound.jsx
+++ src/pages/NotFound/NotFound.jsx
@@ -1,17 +1,21 @@
-import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 import './NotFound.css';
 
 function NotFound() {
+  const navigate = useNavigate();
+
+  const goBack = () => navigate(-1);
+
   return (
     <section className='container container_place_not-found not-found'>
       <span className='not-found__accent'>404</span>
       <h1 className='not-found__title'>Страница не найдена</h1>
-      <Link
-        to={'/'}
-        className='link link_theme_brand-secondary'>
+      <button
+        onClick={goBack}
+        className='not-found__btn hover-effect hover-effect_type_opacity-60'>
         Назад
-      </Link>
+      </button>
     </section>
   );
 }

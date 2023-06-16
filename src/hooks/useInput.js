648c346358437cbdcebff79f825ae5e6f1e9ecb4
Tatiana Ivanova <tat.mi.iva@gmail.com>
2023-06-16 12:15:05 +0300

    feat: add custom hooks for inputs and validation

---
+++ src/hooks/useInput.js
@@ -0,0 +1,13 @@
+import { useState } from 'react';
+
+function useInput(initialValues) {
+  const [values, setValues] = useState(initialValues);
+
+  function handleInputChange(evt) {
+    setValues({ ...values, [evt.target.name]: evt.target.value });
+  }
+
+  return { values, setValues, handleInputChange };
+}
+
+export { useInput };
---
+++ src/hooks/useValidation.js
@@ -0,0 +1,37 @@
+import { useState, useCallback } from 'react';
+
+function useValidation() {
+  const [errorMessages, setErrorMessages] = useState({});
+  const [isFormValid, setIsFormValid] = useState(false);
+  const [isInputsValid, setInputsValidity] = useState({});
+
+  const handleValidityChange = (e) => {
+    setErrorMessages((messages) => ({
+      ...messages,
+      [e.target.name]: e.target.validationMessage,
+    }));
+
+    setInputsValidity((inputs) => ({
+      ...inputs,
+      [e.target.name]: e.target.validity.valid,
+    }));
+
+    setIsFormValid(e.target.closest('.form').checkValidity());
+  };
+
+  const resetValidation = useCallback(() => {
+    setErrorMessages({});
+    setInputsValidity({});
+    setIsFormValid(false);
+  }, []);
+
+  return {
+    errorMessages,
+    isFormValid,
+    isInputsValid,
+    handleValidityChange,
+    resetValidation,
+  };
+}
+
+export { useValidation };

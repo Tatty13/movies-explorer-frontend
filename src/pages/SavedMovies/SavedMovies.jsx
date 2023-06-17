c348157e18b257451bf2198c1e5a5f2196f71d7f
Tatiana Ivanova <tat.mi.iva@gmail.com>
2023-06-17 17:33:51 +0300

    feat: implement functionality to filter movies on the savedMovies page; save movies and filter options in the local storage

--- src/pages/SavedMovies/SavedMovies.jsx
+++ src/pages/SavedMovies/SavedMovies.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 import {
   MoviesCardList,
@@ -6,24 +6,86 @@ import {
   SearchForm,
   SectionWithMovies,
 } from '../../components';
+import { useInput } from '../../hooks';
+import { getDataFromLS, saveDataInLS } from '../../utils/local-storage';
+import { filterMovies } from '../../utils/filterMovies';
 
-function SavedMovies({ isLoading, movies }) {
-  const [isFilterActive, setFilterState] = useState(false);
+function SavedMovies({ movies }) {
+  const [isLoading, setIsLoading] = useState(false);
+  const [isSavedMoviesFilterActive, setFilterState] = useState(false);
+  const [filtredMovies, setFiltredMovies] = useState([]);
+
+  const {
+    values: searchValue,
+    setValues: setSearchValue,
+    handleInputChange,
+  } = useInput({
+    search: '',
+  });
 
   const handleFilterClick = () => {
-    setFilterState(!isFilterActive);
+    setIsLoading(true);
+
+    const filterState = !isSavedMoviesFilterActive;
+    setFilterState(filterState);
+    saveDataInLS({ isSavedMoviesFilterActive: filterState });
+
+    const { savedMoviesSearch } = getDataFromLS('savedMoviesSearch');
+    const filtredMovies = filterMovies(movies, savedMoviesSearch, filterState);
+    setFiltredMovies(filtredMovies);
+
+    setIsLoading(false);
+  };
+
+  const handleSearch = () => {
+    setIsLoading(true);
+
+    const filtedMovies = filterMovies(
+      movies,
+      searchValue.search,
+      isSavedMoviesFilterActive
+    );
+    setFiltredMovies(filtedMovies);
+
+    setIsLoading(false);
   };
 
   function handleSubmit(e) {
     e.preventDefault();
+    handleSearch();
+    saveDataInLS({
+      isSavedMoviesFilterActive,
+      savedMoviesSearch: searchValue.search,
+    });
   }
+
+  useEffect(() => {
+    setIsLoading(true);
+
+    const { isSavedMoviesFilterActive, savedMoviesSearch } = getDataFromLS(
+      'isSavedMoviesFilterActive',
+      'savedMoviesSearch'
+    );
+
+    const booleanFilter = isSavedMoviesFilterActive === 'true';
+    const search = savedMoviesSearch || '';
+    setFilterState(booleanFilter);
+    setSearchValue({ search });
+
+    const filtredMovies = filterMovies(movies, search, booleanFilter);
+    setFiltredMovies(filtredMovies);
+
+    setIsLoading(false);
+  }, [movies, setSearchValue]);
 
   return (
     <SectionWithMovies>
       <SearchForm
         onSubmit={handleSubmit}
-        isFilterActive={isFilterActive}
+        isFilterActive={isSavedMoviesFilterActive}
         onFilterClick={handleFilterClick}
+        searchValue={searchValue.search}
+        onChange={handleInputChange}
       />
 
       {isLoading ? (
@@ -31,7 +93,7 @@ function SavedMovies({ isLoading, movies }) {
       ) : !movies?.length ? (
         <p className='movies__not-found-text'>Ничего не найдено :(</p>
       ) : (
-        <MoviesCardList movies={movies} />
+        <MoviesCardList movies={filtredMovies} />
       )}
     </SectionWithMovies>
   );

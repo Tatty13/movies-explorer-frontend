fd8776d18b42951c50df7b31cd553fa70152a241
Tatiana Ivanova <tat.mi.iva@gmail.com>
2023-06-21 14:15:35 +0300

    refactor: create function to get full assets url on nomoreparties.co api

---
+++ src/utils/helpers/createFullUrl.js
@@ -0,0 +1,9 @@
+/**
+ * @param {string} path - path to asset on api.nomoreparties.co;
+ * @returns
+ */
+function createFullUrl(path) {
+  return `https://api.nomoreparties.co/${path}`;
+}
+
+export { createFullUrl };
--- src/utils/helpers/index.js
+++ src/utils/helpers/index.js
@@ -1,3 +1,4 @@
 export * from './convertMinsToHhMm';
 export * from './filterMovies';
 export * from './local-storage';
+export * from './createFullUrl';

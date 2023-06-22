ecba8103d9ab0958d8feb6fe04d8db74748754f7
Tatiana Ivanova <tat.mi.iva@gmail.com>
2023-06-22 16:42:25 +0300

    refactor: optimize movies render

--- src/components/MoviesCardList/MoviesCardList.jsx
+++ src/components/MoviesCardList/MoviesCardList.jsx
@@ -1,31 +1,28 @@
 import './MoviesCardList.css';
 import { MoviesCard } from '../MoviesCard/MoviesCard';
+import { memo } from 'react';
 
-function MoviesCardList({
-  movies,
-  savedMovies,
-  hasDeleteBtn,
-  onDelete,
-  onSave,
-}) {
-  const moviesList = movies.map((movie) => {
-    const isSaved =
-      hasDeleteBtn ||
-      savedMovies?.some((savedMovie) => savedMovie.movieId === movie.id);
+const MoviesCardList = memo(
+  ({ movies, savedMovies, hasDeleteBtn, onDelete, onSave }) => {
+    const moviesList = movies.map((movie) => {
+      const isSaved =
+        hasDeleteBtn ||
+        savedMovies?.some((savedMovie) => savedMovie.movieId === movie.id);
 
-    return (
-      <li key={movie[savedMovies ? 'id' : '_id']}>
-        <MoviesCard
-          movie={movie}
-          isSaved={isSaved}
-          hasDeleteBtn={hasDeleteBtn}
-          onMovieBtnClick={isSaved ? onDelete : onSave}
-        />
-      </li>
-    );
-  });
+      return (
+        <li key={movie[savedMovies ? 'id' : '_id']}>
+          <MoviesCard
+            movie={movie}
+            isSaved={isSaved}
+            hasDeleteBtn={hasDeleteBtn}
+            onMovieBtnClick={isSaved ? onDelete : onSave}
+          />
+        </li>
+      );
+    });
 
-  return <ul className='list movies__list'>{moviesList}</ul>;
-}
+    return <ul className='list movies__list'>{moviesList}</ul>;
+  }
+);
 
 export { MoviesCardList };

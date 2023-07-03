d52c1fdcc2ef32f9de0ee1a8b691dfbd7a71bfd5
Tatiana Ivanova <tat.mi.iva@gmail.com>
2023-07-04 00:36:29 +0300

    refactor: place promo image and content in the containers, refactor style due to markup changes

--- src/components/Promo/Promo.jsx
+++ src/components/Promo/Promo.jsx
@@ -6,19 +6,22 @@ function Promo() {
   return (
     <section className='promo'>
       <div className='container container_place_promo'>
-        <h1 className='promo__title'>
-          Учебный проект студента факультета{' '}
-          <span className='promo__nospace'>Веб-разработки.</span>
-        </h1>
-        <p className='promo__subtitle'>
-          Листайте ниже, чтобы узнать больше про этот проект и его создателя.
-        </p>
-        <HashLink
-          smooth
-          to='/#about'
-          className='link link_theme_primary promo__link'>
-          Узнать больше
-        </HashLink>
+        <div className='promo__content'>
+          <h1 className='promo__title'>
+            Учебный проект студента факультета{' '}
+            <span className='promo__nospace'>Веб-разработки.</span>
+          </h1>
+          <p className='promo__subtitle'>
+            Листайте ниже, чтобы узнать больше про этот проект и его создателя.
+          </p>
+          <HashLink
+            smooth
+            to='/#about'
+            className='link link_theme_primary promo__link'>
+            Узнать больше
+          </HashLink>
+        </div>
+        <div className='promo__img-container'></div>
       </div>
     </section>
   );
